87e095152868fd1101fe5dc14dd40885229f1f66
radu <radustanciu841@yahoo.ro>
2021-04-08 22:04:44 +0300

    Create ToDoList Component

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,35 @@
+// este o componenta simpla care mapeaza prin Array si afiseaza sub forma de lista todo-urile
+import React from 'react';
+import ToDoItem from './ToDoItem';
+
+function ToDoList(props) {
+    // din obiectul props iau datele deste doto-rui
+    
+    // apelarea functiei de stergere din App
+    function deletList(todoList){
+        props.handleDeletList(todoList);
+    }
+    const{todos}=props;
+    return (
+        <div className='to-do-list'>
+            {/* partea de mapare si afisare sub forma de lista a datelor din Array */}
+            {/* se itereaza array-ul si se returneaza cate un Item, la momentul submiterii formularului */}
+            {
+                todos.map((todo,index)=>{
+                    return <ToDoItem
+                            title={todo.title}
+                            description={todo.description}
+                            date={todo.date}
+                            key={index}
+                            />
+                })
+            }
+            {/* button pentru stergerea listei de todo */}
+            <button onClick={(todoList)=>deletList(todoList)}>Sterge lista de todo</button>
+           
+ 
+        </div>
+    )
+}
+
+export default ToDoList;
